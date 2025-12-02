cad374305e2b1edf741a89d960551415a8552bae
Sara Carreras Camacho <sara.carreras@plainconcepts.com>
2025-12-02 11:20:42 +0100

    add ReactQuery and RQdevtools

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {QueryClientProvider,QueryClient} from "@tanstack/react-query";
+import QueryProvider from "@/components/QueryProvider/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +24,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <QueryProvider >
         {children}
+      </QueryProvider>
       </body>
     </html>
   );
